ff6eb268289cac51952642ebb600549d82c2572b
Fabien Udriot <fabien.udriot@ecodev.ch>
2015-10-22 10:18:41 +0200

    [TASK] Add configuration files

---
+++ ext_emconf.php
@@ -0,0 +1,33 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "speciality".
+ *
+ * Auto generated 10-01-2013 17:54
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 extension to ship common configuration specific to Ecodev',
+	'description' => '',
+	'author' => 'Fabien Udriot',
+	'author_email' => 'support@ecodev.ch',
+	'category' => 'misc',
+	'author_company' => 'Ecodev',
+	'state' => 'stable',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '6.2.0-6.2.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
